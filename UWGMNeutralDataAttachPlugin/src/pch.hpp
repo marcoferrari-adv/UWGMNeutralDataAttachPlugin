8051286561f87239acf177c3a11ad52cfcce9b5d
marcoferrari-adv <m.ferrari@advantage2000.it>
2025-05-26 08:50:19 -0700

    Fixed ifdef for LOG_DEBUG_ERROR_MSG and LOG_DEBUG_WARN_MSG

--- UWGMNeutralDataAttachPlugin/src/pch.hpp
+++ UWGMNeutralDataAttachPlugin/src/pch.hpp
@@ -16,14 +16,14 @@
 #define LOG_DEBUG_MSG(obj, msgFormat, ...)
 #endif
 
-#ifdef DEBUG
+#ifdef DEBUG  && !defined(DISTRIBUTION)
 #define LOG_DEBUG_ERROR_MSG(obj, msgFormat, ...) \
     obj->showMessage(xstring::Printf(msgFormat, __VA_ARGS__), wwgmtkMessageType_Error)
 #else
 #define LOG_DEBUG_ERROR_MSG(obj, msgFormat, ...)
 #endif
 
-#ifdef DEBUG
+#ifdef DEBUG  && !defined(DISTRIBUTION)
 #define LOG_DEBUG_WARN_MSG(obj, msgFormat, ...) \
     obj->showMessage(xstring::Printf(msgFormat, __VA_ARGS__), wwgmtkMessageType_Warning)
 #else
